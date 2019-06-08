3ce101a03523d3d43431adf248f819a8e7427bf6
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2019-06-08 01:57:35 -0500

    Updated to version 0.1.1.

--- moltransform/io.py
+++ moltransform/io.py
@@ -16,39 +16,35 @@ def read_xyz(filename):
 	Returns
 	-------
 	numpy.ndarray
-		A numpy ndarray object for a posterior transformation.
+		A NumPy ndarray object for a posterior transformation.
     """
 	with open(filename, 'r') as xyz:
-		n_lns = int(xyz.readline())
-		p_mat = np.zeros((20, 3))
-		for l in range(n_lns + 1):
-			ps = xyz.readline().split(" ")
-			if l > 0:
-				ps[-1] = ps[-1][:-1]
-				ps = list(filter(lambda x: x != "" and x != "\n", ps))
-				p_mat[l - 1] = np.array([float(ps[1]), float(ps[2]), float(ps[3])])
-	return p_mat
+		total_lines = int(xyz.readline())
+		position_matrix = np.zeros((total_lines, 3))
+		for line in range(total_lines + 1):
+			pos = xyz.readline().split(" ")
+			if line > 0:
+				pos[-1] = pos[-1][:-1]
+				pos = list(filter(lambda x: x != "" and x != "\n", pos))
+				pos_line = [float(pos[1]), float(pos[2]), float(pos[3])]
+				position_matrix[line - 1] = np.array(pos_line)
+	return position_matrix
 
 
-def write_xyz(filename, pos_mat):
+def write_xyz(filename, pos_matrix):
 	"""Utility function to write a xyz file.
 
 	Parameters
 	----------
 	filename : str
 		Path to xyz file to save.
-	pos_mat : numpy.ndarray
-		A numpy ndarray object to save into a file.
-
-	Returns
-	-------
-	numpy.ndarray
-		A numpy ndarray object for a posterior transformation.
+	pos_matrix : numpy.ndarray
+		A NumPy ndarray object to save into a file.
 	"""
 	with open(filename, 'w') as xyz:
-		xyz.write("{}\n".format(len(pos_mat)))
+		xyz.write("{}\n".format(len(pos_matrix)))
 		xyz.write("\n")
-		for line in range(len(pos_mat)):
+		for line in range(len(pos_matrix)):
 			xyz.write("C\t\t {: .5f}\t {: .5f}\t {: .5f}\n".format(
-				pos_mat[line, 0], pos_mat[line, 1], pos_mat[line, 2]))
+				pos_matrix[line, 0], pos_matrix[line, 1], pos_matrix[line, 2]))
 	return
--- moltransform/transform.py
+++ moltransform/transform.py
@@ -5,49 +5,99 @@ TODO: Add description.
 import numpy as np
 
 
-def center(p_mat):
-    """
-    Utility function to center a set of 3D coordinates.
-    """
-    print("(WARNING) Not single transformation selected.")
-    print("(INFO) Molecule centering to be done.")
-    min_x, max_x = np.min(p_mat[:, 0]), np.max(p_mat[:, 0])
-    min_y, max_y = np.min(p_mat[:, 1]), np.max(p_mat[:, 1])
-    min_z, max_z = np.min(p_mat[:, 2]), np.max(p_mat[:, 2])
-    mean_x = (max_x + min_x) / 2
-    mean_y = (max_y + min_y) / 2
-    mean_z = (max_z + min_z) / 2
-    t_vec = [-mean_x, -mean_y, -mean_z]
-    t_mat = translate(p_mat, t_vec)
-    return t_mat
+def center(pos_matrix, verbose=False):
+	"""Utility function to center a set of 3D coordinates.
+
+	Parameters
+	----------
+	pos_matrix : numpy.ndarray
+		A NumPy ndarray with centered molecules.
+	verbose : bool
+		A flag to print the transformation matrix to be used.
+
+	Returns
+	-------
+	numpy.ndarray
+		A NumPy ndarray with centered xyz positions.
+	"""
+	if verbose:
+		print("(INFO) Molecule centering to be done.")
+	min_x, max_x = np.min(pos_matrix[:, 0]), np.max(pos_matrix[:, 0])
+	min_y, max_y = np.min(pos_matrix[:, 1]), np.max(pos_matrix[:, 1])
+	min_z, max_z = np.min(pos_matrix[:, 2]), np.max(pos_matrix[:, 2])
+	mean_x = (max_x + min_x) / 2
+	mean_y = (max_y + min_y) / 2
+	mean_z = (max_z + min_z) / 2
+	t_vector = [-mean_x, -mean_y, -mean_z]
+	t_matrix = translate(pos_matrix, t_vector, verbose)
+	return t_matrix
 
 
-def translate(p_mat, translation_vec=None):
-    """
-    Utility function to translate a set of 3D coordinates.
-    """
-    T = np.eye(4)
-    T[:-1, 3] = translation_vec
-    print("(INFO) Transformation matrix to be used:")
-    print(T)
-    t_mat = p_mat.copy()
-    for row in range(len(p_mat)):
-        t_mat[row] = (T @ np.hstack((p_mat[row], [1])))[:-1]
-    return t_mat
+def translate(pos_matrix, translation_vector=None, verbose=False):
+	"""Utility function to translate a set of 3D coordinates.
+
+	Parameters
+	----------
+	pos_matrix : numpy.ndarray
+		A NumPy ndarray with centered molecules.
+	translation_vector : numpy.array or list
+		The translation vector to be used.
+	verbose : bool
+		A flag to print the transformation matrix to be used.
+
+	Returns
+	-------
+	numpy.ndarray
+		A NumPy ndarray with translated xyz positions.
+	"""
+	if translation_vector is None:
+		if verbose:
+			print("(INFO) No translation vector was defined.")
+		return pos_matrix
+	else:
+		T = np.eye(4)
+		T[:-1, 3] = translation_vector
+
+		if verbose:
+			print("(INFO) Transformation matrix to be used:")
+			print(T)
+
+		t_matrix = pos_matrix.copy()
+		for row in range(len(pos_matrix)):
+			t_matrix[row] = (T @ np.hstack((pos_matrix[row], [1])))[:-1]
+		return t_matrix
 
 
-def scale(p_mat, scaling_vec=None):
-    """
-    Utility function to rotate a set of 3D coordinates.
-    """
-    T = np.eye(4)
-    if scaling_vec is None:
-        return p_mat
-    else:
-        T[:-1, :-1] *= scaling_vec
-        print("(INFO) Transformation matrix to be used:")
-        print(T)
-    t_mat = p_mat.copy()
-    for row in range(len(p_mat)):
-        t_mat[row] = (T @ np.hstack((p_mat[row], [1])))[:-1]
-    return t_mat
+def scale(pos_matrix, scaling_vector=None, verbose=False):
+	"""Utility function to rotate a set of 3D coordinates.
+
+	Parameters
+	----------
+	pos_matrix : numpy.ndarray
+		A NumPy ndarray with centered molecules.
+	scaling_vector : numpy.array or list
+		The scaling vector to be used.
+	verbose : bool
+		A flag to print the transformation matrix to be used.
+
+	Returns
+	-------
+	numpy.ndarray
+		A NumPy ndarray with scaled xyz positions.
+	"""
+	if scaling_vector is None:
+		if verbose:
+			print("(INFO) No scaling vector was defined.")
+		return pos_matrix
+	else:
+		T = np.eye(4)
+		T[:-1, :-1] *= scaling_vector
+
+		if verbose:
+			print("(INFO) Transformation matrix to be used:")
+			print(T)
+
+		t_matrix = pos_matrix.copy()
+		for row in range(len(pos_matrix)):
+			t_matrix[row] = (T @ np.hstack((pos_matrix[row], [1])))[:-1]
+		return t_matrix
